9780c5172e1abd663afdc70d32260bf150f0d003
Justin Hooper <justin_hooper@yahoo.com>
2019-06-08 14:40:40 +0000

    Lesson 26 recommit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], per_page: 10)
@@ -10,9 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   def show
     @place = Place.find(params[:id])
@@ -32,21 +36,27 @@ class PlacesController < ApplicationController
   end
 
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
   end
+end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+    private
+
     @place.destroy
     redirect_to root_path
   end
 
-  private
-
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-end
-end
 
-
+end
