5d9b951fa86abb3c4dbcf6ac6161c67b64ca6e14
Justin Hooper <justin_hooper@yahoo.com>
2019-06-08 17:11:51 +0000

    Lesson 20 geocoding set up

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+
+  
+  validates :name, presence: true, length: { minimum: 3, too_long: "3 characters is the minimum allowed"}
+  validates :address, presence: true
+  validates :description, presence: true
 end
